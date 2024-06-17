205b3ddb28f0a2ef63d0d2e447004736b186775e
Monika Lenartowicz <nika.nel@interia.pl>
2024-06-17 13:31:54 +0200

    added dark theme functionality

--- src/context.jsx
+++ src/context.jsx
@@ -3,8 +3,16 @@ import { useState, useContext, useEffect, createContext } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-	const greeting = "hello";
-	return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+	const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+	const toggleDarkTheme = () => {
+		const newDarkTheme = !isDarkTheme;
+		setIsDarkTheme(newDarkTheme);
+		const body = document.querySelector("body");
+		body.classList.toggle("dark-theme", newDarkTheme);
+	};
+
+	return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(AppContext);
