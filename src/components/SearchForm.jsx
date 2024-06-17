89a3760cbcfa8b0afeda81ad767769a844a01032
Monika Lenartowicz <nika.nel@interia.pl>
2024-06-17 14:06:48 +0200

    Create a form with an input and a submit button.

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,18 @@
+const SearchForm = () => {
+	const handlSubmit = e => {
+		e.preventDefault();
+		const searchValue = e.target.elements.search.value;
+		console.log(searchValue);
+	};
+	return (
+		<section>
+			<h1 className='title'>unsplash images</h1>
+			<form className='search-form' onSubmit={handlSubmit}>
+				<input type='text' name='search' className='form-input search-input' placeholder='cat' />
+				<button className='btn'>search</button>
+			</form>
+		</section>
+	);
+};
+
+export default SearchForm;
